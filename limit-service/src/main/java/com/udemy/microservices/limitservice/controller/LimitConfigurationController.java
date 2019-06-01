4540a1dfc323d8a52c7678f6a0aaaff1c7b6a830
Ferlim Putra <f.putra@allegrodevelopment.com>
2019-06-01 22:02:46 +0700

    Finishing the courses.
    Added Zipkin
    Added Hystrix
    Added Spring Cloud Bus

--- limit-service/src/main/java/com/udemy/microservices/limitservice/controller/LimitConfigurationController.java
+++ limit-service/src/main/java/com/udemy/microservices/limitservice/controller/LimitConfigurationController.java
@@ -1,5 +1,6 @@
 package com.udemy.microservices.limitservice.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.udemy.microservices.limitservice.bean.LimitConfigurationBean;
 import com.udemy.microservices.limitservice.config.LimitConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +19,15 @@ public class LimitConfigurationController {
   private LimitConfiguration config;
 
   @GetMapping("/config")
+  @HystrixCommand(fallbackMethod = "getConfigFallback")
   public LimitConfigurationBean getConfig() {
+    //throw new RuntimeException("not available");
     return new LimitConfigurationBean(config.getMinimum(), config.getMaximum());
+  }
+
+  @GetMapping("/config/fallback")
+  public LimitConfigurationBean getConfigFallback() {
+    return new LimitConfigurationBean(9, 99);
   }
 
 }
